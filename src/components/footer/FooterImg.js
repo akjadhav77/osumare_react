15a750c45b0e44899c4c9573c0e2c6e9ed52ab92
Akshay <erakshayjadhav8@gmail.com>
2023-03-05 18:18:48 +0530

    final

--- src/components/footer/FooterImg.js
+++ src/components/footer/FooterImg.js
@@ -3,11 +3,13 @@ import './FooterImg.css'
 
 const FooterImg = () => {
   return (
-    <div className='footerimg_flex'>
-        <div className='footerimg_content'>
-            <img src="./assets/images/asset_footer.png" alt="" />
-            <h5>© 2021 Osumare. All rights reserved.</h5>
-        </div>
+    <div className="background_clr2">
+      <div className='footerimg_flex'>
+          <div className='footerimg_content'>
+              <img src="./assets/images/asset_footer.png" alt="" />
+              <h5>© 2021 Osumare. All rights reserved.</h5>
+          </div>
+      </div>
     </div>
   )
 }
